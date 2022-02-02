087e1baa844e4c4a963c0721f306a875283f022a
Mukund Gupta <6190006@itisrossi.vi.it>
2022-02-02 16:13:11 +0100

    final

--- src/index.js
+++ src/index.js
@@ -8,18 +8,80 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var _a;
 const DOGS_BOX = document.getElementById("dogs_box");
+const IMAGE_BOX = document.getElementById("image");
+var ARROWS = [];
+ARROWS.push(document.getElementById("left_arrow"));
+ARROWS.push(document.getElementById("right_arrow"));
 window.onload = () => __awaiter(void 0, void 0, void 0, function* () {
     let names = yield fetchDogs("https://dog.ceo/api/breeds/list/all");
     for (let name in names) {
-        console.log(name);
         let tmp = document.createElement("option");
         tmp.innerText = name;
         DOGS_BOX.append(tmp);
     }
+    console.log(ARROWS[1]);
+    ARROWS.forEach((el) => el.addEventListener("click", (e) => {
+        Slider.useSlider(e.target);
+    }));
+    DOGS_BOX.addEventListener("change", (event) => {
+        Slider.hasAlreadyChanged++;
+        IMAGE_BOX.innerText = "";
+        Slider.displayImages(event.target);
+    });
 });
 const fetchDogs = (url) => __awaiter(void 0, void 0, void 0, function* () {
     let result = yield fetch(url);
     let data = yield result.json();
     return data.message;
+});
+const fetchImages = (url) => __awaiter(void 0, void 0, void 0, function* () {
+    let result = yield fetch(url);
+    let data = yield result.json();
+    return yield data.message.splice(0, 10);
+});
+class Slider {
+    static addClasses() {
+        ARROWS.forEach((el) => {
+            el.classList.add("arrow");
+        });
+        console.log(ARROWS[0].className);
+    }
+    static useSlider(arrow) {
+        if (this.result.length == 0) {
+            return;
+        }
+        switch (arrow.id) {
+            case "left_arrow":
+                if (this.image == 0) {
+                    this.image = this.result.length - 1;
+                }
+                else {
+                    this.image--;
+                }
+                break;
+            case "right_arrow":
+                if (this.image == this.result.length - 1) {
+                    this.image = 0;
+                }
+                else {
+                    this.image++;
+                }
+                break;
+            default:
+                break;
+        }
+        IMAGE_BOX.style.backgroundImage = `url(${this.result[this.image]})`;
+    }
+}
+_a = Slider;
+Slider.image = 0;
+Slider.result = [];
+Slider.hasAlreadyChanged = 0;
+Slider.displayImages = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    _a.image = 0;
+    _a.hasAlreadyChanged <= 1 && _a.addClasses();
+    _a.result = yield fetchImages(`https://dog.ceo/api/breed/${event.value}/images`);
+    IMAGE_BOX.style.backgroundImage = `url(${_a.result[0]})`;
 });
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 const DOGS_BOX: HTMLSelectElement = document.getElementById(
   "dogs_box"
 ) as HTMLSelectElement;
+const IMAGE_BOX: HTMLDivElement = document.getElementById(
+  "image"
+) as HTMLDivElement;
+var ARROWS: HTMLSpanElement[] = [];
+ARROWS.push(document.getElementById("left_arrow") as HTMLSpanElement);
 
+ARROWS.push(document.getElementById("right_arrow") as HTMLSpanElement);
 window.onload = async () => {
   let names: { [key: string]: string } = await fetchDogs(
     "https://dog.ceo/api/breeds/list/all"
   );
-
   for (let name in names) {
-    console.log(name);
     let tmp = document.createElement("option");
     tmp.innerText = name;
     DOGS_BOX.append(tmp);
   }
+  console.log(ARROWS[1]);
+  ARROWS.forEach((el) =>
+    el.addEventListener("click", (e) => {
+      Slider.useSlider(e.target as HTMLSpanElement);
+    })
+  );
+  DOGS_BOX.addEventListener("change", (event) => {
+    Slider.hasAlreadyChanged++;
+    IMAGE_BOX.innerText = "";
+    Slider.displayImages(event.target as HTMLSelectElement);
+  });
 };
 
 const fetchDogs = async (url: string) => {
@@ -20,5 +35,54 @@ const fetchDogs = async (url: string) => {
   let data = await result.json();
   return data.message;
 };
+const fetchImages = async (url: string) => {
+  let result = await fetch(url);
+  let data = await result.json();
+  return await (<string[]>data.message).splice(0, 10);
+};
 
-console.log("prova");
+class Slider {
+  private static image: number = 0;
+  private static result: string[] = [];
+  public static hasAlreadyChanged: number = 0;
+  static displayImages = async (event: HTMLSelectElement) => {
+    this.image = 0;
+    this.hasAlreadyChanged <= 1 && this.addClasses();
+
+    this.result = await fetchImages(
+      `https://dog.ceo/api/breed/${event.value}/images`
+    );
+
+    IMAGE_BOX.style.backgroundImage = `url(${this.result[0]})`;
+  };
+  private static addClasses() {
+    ARROWS.forEach((el) => {
+      el.classList.add("arrow");
+    });
+    console.log(ARROWS[0].className);
+  }
+  static useSlider(arrow: HTMLSpanElement) {
+    if (this.result.length == 0) {
+      return;
+    }
+    switch (arrow.id) {
+      case "left_arrow":
+        if (this.image == 0) {
+          this.image = this.result.length - 1;
+        } else {
+          this.image--;
+        }
+        break;
+      case "right_arrow":
+        if (this.image == this.result.length - 1) {
+          this.image = 0;
+        } else {
+          this.image++;
+        }
+        break;
+      default:
+        break;
+    }
+    IMAGE_BOX.style.backgroundImage = `url(${this.result[this.image]})`;
+  }
+}
